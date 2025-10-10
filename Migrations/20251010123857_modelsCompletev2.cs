56c97e560b8298eeb3c3292183da8730eb3556e4
Developer Backend Node <160889254+c-correa@users.noreply.github.com>
2025-10-10 07:40:29 -0500

    Add migrations for doctor and appointment models
    Introduces new migration files to support doctor, appointment, availability schedule, doctor details, and history record tables. These changes expand the database schema to enable management of doctors, their schedules, appointments, and related records.

---
+++ Migrations/20251010123857_modelsCompletev2.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ApiTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class modelsCompletev2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "availability_schedule",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    hour = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    monday = table.Column<bool>(type: "boolean", nullable: false),
+                    tuesday = table.Column<bool>(type: "boolean", nullable: false),
+                    wednesday = table.Column<bool>(type: "boolean", nullable: false),
+                    thursday = table.Column<bool>(type: "boolean", nullable: false),
+                    friday = table.Column<bool>(type: "boolean", nullable: false),
+                    saturday = table.Column<bool>(type: "boolean", nullable: false),
+                    sunday = table.Column<bool>(type: "boolean", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    update_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_availability_schedule", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "doctor_details",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    doctor_id = table.Column<int>(type: "integer", nullable: false),
+                    license_number = table.Column<string>(type: "text", nullable: false),
+                    specialty = table.Column<string>(type: "text", nullable: false),
+                    years_experience = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    update_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_doctor_details", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "doctors",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    first_name = table.Column<string>(type: "text", nullable: false),
+                    last_name = table.Column<string>(type: "text", nullable: false),
+                    phone = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    is_active = table.Column<bool>(type: "boolean", nullable: false),
+                    password = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    update_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_doctors", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "appointments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    appointment_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    appointment_time = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    notes = table.Column<string>(type: "text", nullable: true),
+                    pet_id = table.Column<int>(type: "integer", nullable: false),
+                    doctor_id = table.Column<int>(type: "integer", nullable: false),
+                    VeterinarianId = table.Column<int>(type: "integer", nullable: false),
+                    history_record_id = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    update_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_appointments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_appointments_Pets_pet_id",
+                        column: x => x.pet_id,
+                        principalTable: "Pets",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_appointments_doctors_VeterinarianId",
+                        column: x => x.VeterinarianId,
+                        principalTable: "doctors",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "history_record",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AppointmentsModelId = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    update_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_history_record", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_history_record_appointments_AppointmentsModelId",
+                        column: x => x.AppointmentsModelId,
+                        principalTable: "appointments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_appointments_pet_id",
+                table: "appointments",
+                column: "pet_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_appointments_VeterinarianId",
+                table: "appointments",
+                column: "VeterinarianId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_history_record_AppointmentsModelId",
+                table: "history_record",
+                column: "AppointmentsModelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "availability_schedule");
+
+            migrationBuilder.DropTable(
+                name: "doctor_details");
+
+            migrationBuilder.DropTable(
+                name: "history_record");
+
+            migrationBuilder.DropTable(
+                name: "appointments");
+
+            migrationBuilder.DropTable(
+                name: "doctors");
+        }
+    }
+}
