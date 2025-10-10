c2425932ae88ca29c33db43af27fc409ce47f2a5
Developer Backend Node <160889254+c-correa@users.noreply.github.com>
2025-10-10 07:41:52 -0500

    Refactor AppointmentsController to use BaseController
    AppointmentsController now inherits from BaseController<AppointmentsModel> and removes redundant action methods. Also, unnecessary using directives were cleaned up in both controller and model files.

--- Src/Appointments/AppointmentsControllers.cs
+++ Src/Appointments/AppointmentsControllers.cs
@@ -1,37 +1,15 @@
 
-using ApiTest.Src.Appointments;
 using ApiTest.Utils;
 using Microsoft.AspNetCore.Mvc;
 
 
-namespace ApiTest.Src.Doctors
+namespace ApiTest.Src.Appointments
 {
-    [ApiController]
     [Route("api/appointments")]
-    public class AppointmentsController(ServicesAppointments servicesAppointments) : ControllerBase
+    public class AppointmentsController(ServicesAppointments servicesAppointments) : BaseController<AppointmentsModel>(servicesAppointments)
     {
         private readonly ServicesAppointments _servicesAppointments = servicesAppointments;
 
-        [HttpGet]
-        public IActionResult Get()
-        {
-            var result = SafeExecutor.Execute(() => _servicesAppointments.GetAll());
-            return Ok(result);
-        }
-
-        [HttpGet("{id}")]
-        public IActionResult GetOne(int id)
-        {
-            var result = SafeExecutor.Execute(() => _servicesAppointments.GetById(id));
-            return Ok(result);
-        }
-        
-        [HttpPost]
-        public IActionResult Create([FromBody] AppointmentsModel data)
-        {
-                var result = SafeExecutor.Execute(() => _servicesAppointments.Add(data));
-                return Ok(result);
-        }
     }
 
 }
