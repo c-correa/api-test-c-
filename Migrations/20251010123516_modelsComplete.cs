186ccf6f6354201d4f4ebe82d41b9d0c001220bb
Developer Backend Node <160889254+c-correa@users.noreply.github.com>
2025-10-10 07:40:21 -0500

    Add new models and update migration for database
    Added DbSet properties for Appointments, AvailabilitySchedule, Doctors, DoctorDetails, and HistoryRecords to DBContext. Created migration to update owners and pets tables, including renaming and adding new columns for improved data structure.

---
+++ Migrations/20251010123516_modelsComplete.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class modelsComplete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "owners",
+                newName: "phone");
+
+            migrationBuilder.AddColumn<string>(
+                name: "bred",
+                table: "Pets",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "name",
+                table: "Pets",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "sexo",
+                table: "Pets",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "email",
+                table: "owners",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "first_name",
+                table: "owners",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "last_name",
+                table: "owners",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "bred",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "name",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "sexo",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "email",
+                table: "owners");
+
+            migrationBuilder.DropColumn(
+                name: "first_name",
+                table: "owners");
+
+            migrationBuilder.DropColumn(
+                name: "last_name",
+                table: "owners");
+
+            migrationBuilder.RenameColumn(
+                name: "phone",
+                table: "owners",
+                newName: "name");
+        }
+    }
+}
--- data/DBContext.cs
+++ data/DBContext.cs
@@ -3,6 +3,11 @@ using DotNetEnv;
 using ApiTest.Src.Owners;
 using ApiTest.Src.Pets;
 using ApiTest.Src.OwnerPets;
+using ApiTest.Src.Appointments;
+using ApiTest.Src.AvailabilitySchedule;
+using ApiTest.Src.Doctors;
+using ApiTest.Src.DoctorDetails;
+using ApiTest.Src.HistoryRecords;
 
 namespace ApiTest.Data
 {
@@ -28,6 +33,11 @@ namespace ApiTest.Data
         public DbSet<OwnerModel> Owners { get; set; }
         public DbSet<PetModel> Pets { get; set; }
         public DbSet<OwnerPetsModel> OwnersPets { get; set; }
+        public DbSet<AppointmentsModel> Appointments { get; set; }
+        public DbSet<AvailabilityScheduleModel> AvailabilitySchedule { get; set; }
+        public DbSet<DoctorModel> Doctors { get; set; }
+        public DbSet<DoctorDetailsModel> DoctorDetails { get; set; }
+        public DbSet<HistoryRecordModel> HistoryRecords { get; set; }
 
 
     }
