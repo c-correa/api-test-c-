a8e1048e16ba396a6ee0b5394c50402868a29edb
Developer Backend Node <160889254+c-correa@users.noreply.github.com>
2025-10-10 07:41:08 -0500

    Update build cache and assembly info
    Regenerated build output files with updated input hashes and informational version in AssemblyInfo.cs, reflecting the latest build state.

--- obj/Debug/net8.0/test.AssemblyInfo.cs
+++ obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7267b170d3bf8ca752a2fde8dfac52c0a29c884c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3280aac8a28c0629db31cd6d611a583d81fcc602")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
